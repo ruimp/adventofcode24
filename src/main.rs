2eb7c92ff2e846c2cdb51e02829c50c763a82a7c
ruimp <ruimpxt@gmail.com>
2025-06-04 14:28:44 -0400

    Finish day 1

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 
 fn main() -> Result<()> {
-    // let a = vec![3, 4, 2, 1, 3, 3];
-    // let b = vec![4, 3, 5, 3, 9, 3];
+    // let file_path = String::from("test_input.txt");
     let file_path = String::from("input.txt");
     let input = get_input(file_path)?;
 
+    // let distance = dist(&input.0, &input.1);
+    // println!("{}", distance);
+
     let distance = dist(&input.0, &input.1);
-    println!("{}", distance);
+    let similarity = simi(&input.0, &input.1);
+    println!("Distance: {}", distance);
+    println!("Similarity: {}", similarity);
     Ok(())
 }
 
@@ -16,8 +20,8 @@ fn main() -> Result<()> {
 fn get_input(file_path: String) -> Result<(Vec<i32>, Vec<i32>)> {
     let file = File::open(file_path)?;
     let buf_reader = BufReader::new(file);
-    let mut x: Vec<i32> = Vec::new();
-    let mut y: Vec<i32> = Vec::new();
+    let mut a: Vec<i32> = Vec::new();
+    let mut b: Vec<i32> = Vec::new();
     for line in buf_reader.lines() {
         let line = line?;
         let nums: Vec<i32> = line
@@ -25,21 +29,30 @@ fn get_input(file_path: String) -> Result<(Vec<i32>, Vec<i32>)> {
             .filter_map(|s| s.parse::<i32>().ok())
             .collect();
 
-        x.push(nums[0]);
-        y.push(nums[1]);
+        a.push(nums[0]);
+        b.push(nums[1]);
     }
-    Ok((x, y))
+    Ok((a, b))
 }
 
 // Find distance between vectors by summing absolute element-wise difference
-fn dist(x: &Vec<i32>, y: &Vec<i32>) -> i32 {
-    let mut x_sorted = x.clone();
-    x_sorted.sort();
-    let mut y_sorted = y.clone();
-    y_sorted.sort();
-    x_sorted
+fn dist(a: &Vec<i32>, b: &Vec<i32>) -> i32 {
+    let mut a_sorted = a.clone();
+    a_sorted.sort();
+    let mut b_sorted = b.clone();
+    b_sorted.sort();
+    a_sorted
         .iter()
-        .zip(y_sorted.iter())
+        .zip(b_sorted.iter())
         .map(|(&x, &y)| (x - y).abs())
         .sum()
+}
+
+fn simi(a: &Vec<i32>, b: &Vec<i32>) -> i32 {
+    let mut scores: Vec<i32> = Vec::new();
+    for x in a.iter() {
+        let count = b.iter().filter(|&y| x == y).count() as i32;
+        scores.push(count * x);
+    }
+    scores.iter().sum()
 }
