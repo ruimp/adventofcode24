1aa61a6da7b9da582d4535df1750fee64bcfe9d1
ruimp <ruimpxt@gmail.com>
2025-06-05 11:56:24 -0400

    Finish day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,14 +1,15 @@
-// use regex::Regex;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 
 fn main() -> Result<()> {
-    let file_path = String::from("test_input2.txt");
-    // let file_path = String::from("input.txt");
+    // let file_path = String::from("test_input2.txt");
+    let file_path = String::from("input.txt");
     let data = read_file(file_path)?;
-    // println!("{:?}", data);
-    let total = proc(&data);
-    println!("{:?}", total);
+    let total = parse(&data);
+    println!("Total: {:?}", total);
+    let chunks = val_split(&data);
+    let valid_total = val_parse(chunks);
+    println!("Valid total: {:?}", valid_total);
     Ok(())
 }
 
@@ -31,9 +32,36 @@ fn mul(s: &str) -> Option<u32> {
     Some(x * y)
 }
 
-fn proc(s: &str) -> u32 {
+fn parse(s: &str) -> u32 {
     let chunks: Vec<&str> = s.split("mul(").collect();
     let muls: Vec<u32> = chunks.iter().filter_map(|s| mul(s)).collect();
-    // println!("{:?}", muls);
     muls.iter().sum()
+}
+
+fn val_split(s: &str) -> Vec<&str> {
+    let mut chunks: Vec<&str> = Vec::new();
+    let do_chunks: Vec<&str> = s.split_inclusive("do()").collect();
+    for do_chunk in do_chunks.iter() {
+        let dont_chunk: Vec<&str> = do_chunk.split_inclusive("don't()").collect();
+        for chunk in dont_chunk.iter() {
+            chunks.push(&chunk)
+        }
+    }
+    chunks
+}
+
+fn val_parse(ss: Vec<&str>) -> u32 {
+    let mut valid: bool = true;
+    let mut total: u32 = 0;
+    for s in ss.iter() {
+        if valid {
+            total += parse(s);
+        }
+        if s.ends_with("don't()") {
+            valid = false;
+        } else if s.ends_with("do()") {
+            valid = true;
+        }
+    }
+    total
 }
